5f1fd7c577ee99290521c05faf446ba9c0236718
Kutay Keleş <kutaykeles@msn.com>
2024-08-12 23:52:09 +0300

    Coded the DDD abstractions and Models for  ordering microservice in Ordering.API

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
+using Ordering.Domain.Enums;
+
+namespace Ordering.Domain.Models;
+public class Order : Aggregate<Guid>
+{
+    private readonly List<OrderItem> orderItems = new();
+    public IReadOnlyList<OrderItem> OrderItems => orderItems.AsReadOnly();
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x=>x.Price * x.Quantity);
+        private set { }
+    }
+
+}
