c4de1cff07be85e868df2f56f3d9debc094de9d8
kutaykeles <kutaykeles@msn.com>
2024-08-14 18:49:08 +0300

    Infrastructure configs

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 namespace Ordering.Infrastructure;
 public static class DependencyInjection
 {
@@ -7,7 +9,11 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
+        // Add services to the container.
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
 
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
     }
